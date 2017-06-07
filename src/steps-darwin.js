e0471934b8fdb08a0aea01f825051a82e995db1d
Tom J Nowell <contact@tomjn.com>
2017-06-07 15:56:43 +0100

    adds the correct box on MacOS

--- src/steps-darwin.js
+++ src/steps-darwin.js
@@ -39,7 +39,7 @@ var steps = [
   {
     "label": "Adding VVV Box",
     "type": "exec",
-    "exec": "/usr/local/bin/vagrant box add ubuntu/trusty64 vvv-contribute.box"
+    "exec": "/usr/local/bin/vagrant box add ubuntu/trusty64 vvv.box"
   },
   {
     "label": "Starting VVV for the first time",

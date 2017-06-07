7190c9fc0019fa6915232f0dc0e9eb1296613f09
Tom J Nowell <contact@tomjn.com>
2017-06-07 13:41:42 +0100

    swaps out steps to use absolute paths on MacOS as sometimes it doesn't pick up that vbox or vagrant are installed

--- src/steps-darwin.js
+++ src/steps-darwin.js
@@ -2,22 +2,22 @@ var steps = [
   {
     "label": "Mounting VirtualBox disk",
     "type": "exec",
-    "exec": "if ! hash vbox-img 2>/dev/null; then hdiutil attach MacOS/virtualbox.dmg; fi"
+    "exec": "if ! -f /usr/local/bin/vbox-img; then hdiutil attach MacOS/virtualbox.dmg; fi"
   },
   {
     "label": "Installing VirtualBox",
     "type": "exec",
-    "exec": "if ! hash vbox-img 2>/dev/null; then open -W /Volumes/VirtualBox/VirtualBox.pkg; fi"
+    "exec": "if ! -f /usr/local/bin/vbox-img; then open -W /Volumes/VirtualBox/VirtualBox.pkg; fi"
   },
   {
     "label": "Mounting Vagrant disk",
     "type": "exec",
-    "exec": "if ! hash vagrant 2>/dev/null; then hdiutil attach MacOS/vagrant.dmg; fi"
+    "exec": "if ! -f /usr/local/bin/vagrant; then hdiutil attach MacOS/vagrant.dmg; fi"
   },
   {
     "label": "Installing Vagrant",
     "type": "exec",
-    "exec": "if ! hash vagrant 2>/dev/null; then open -W /Volumes/Vagrant/vagrant.pkg; fi"
+    "exec": "if ! -f /usr/local/bin/vagrant; then open -W /Volumes/Vagrant/vagrant.pkg; fi"
   },
   {
     "label": "Installing Vagrant Hosts Updater Plugin",

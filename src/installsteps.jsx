320c002b9c0b1031aa170286d0ae2122a0ba739f
Tom J Nowell <contact@tomjn.com>
2017-06-09 15:27:45 +0100

    show the install location when installing

--- src/installsteps.jsx
+++ src/installsteps.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProgressBar from './progressbar';
 import ProgressStatus from './progressstatus';
+var path = require('path');
 
 class InstallSteps extends React.Component {
 	constructor(props) {
 		super(props);
+		this.install_path = path.join(process.env.HOME, 'vagrant-local');
 	}
 	render() {
 		return (
@@ -13,6 +15,7 @@ class InstallSteps extends React.Component {
 					<ProgressBar progress={this.props.progress} />
 					<ProgressStatus message={this.props.message} />
 					<p>We're going to do some checks and install some things!</p>
+					<p>When done, VVV will be installed at {this.install_path}</p>
 				</div>
 			</div>
 		);

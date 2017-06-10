bb0cf66df0b907ae932fcf85f3c82e675c3a1498
Tom J Nowell <contact@tomjn.com>
2017-06-10 16:08:19 +0100

    moves the windows commands to look one folder up

--- src/steps-win32.js
+++ src/steps-win32.js
@@ -10,40 +10,35 @@ var steps = [
   {
     "label": "Installing VirtualBox",
     "type": "exec",
-    "exec": "IF NOT EXIST vbox-img VirtualBox.exe"
+    "exec": "IF NOT EXIST vbox-img ..\\VirtualBox.exe"
   },
   {
     "label": "Installing Vagrant",
     "type": "exec",
-    "exec": "Vagrant.msi"
+    "exec": "..\\Vagrant.msi"
   },
   {
     "label": "Installing Vagrant Hosts Updater Plugin",
     "type": "exec",
-    "exec": "vagrant plugin install vagrant-hostsupdater"
-  },
-  {
-    "label": "Installing Vagrant Triggers Plugin",
-    "type": "exec",
-    "exec": "vagrant plugin install vagrant-triggers"
+    "exec": "vagrant plugin install vagrant-hostsupdater vagrant-triggers"
   },
   {
     "label": "Extracting VVV archive",
     "type": "unzip",
     "test": install_path + "vvv-config.yml",
-    "source": "..\\vvv.zip",
+    "source": "..\\..\\vvv.zip",
     "target": install_path
   },
   {
     "label": "Adding VVV Box",
     "type": "exec",
-    "exec": "vagrant box list | findstr \"ubuntu/trusty64\" 1>nul || vagrant box add ubuntu/trusty64 ..\\vvv.box"
-  },
+    "exec": "vagrant box list | findstr \"ubuntu/trusty64\" 1>nul || vagrant box add ubuntu/trusty64 ..\\..\\vvv.box"
+  }/*,
   {
     "label": "Starting VVV for the first time",
     "type": "exec",
     "exec": "vagrant up --provider virtualbox"
-  }
+  }*/
 ];
 
 module.exports = steps;
